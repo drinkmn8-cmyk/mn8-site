f95e2b20e57bf906fbe67f0278cbba7c0baa5f95
drinkmn8-cmyk <drinkmn8@gmail.com>
2026-01-26 16:53:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,5 +13,20 @@
   window.addEventListener("scroll", update, { passive: true });
   window.addEventListener("resize", update);
   update();
+
+(() => {
+  const els = document.querySelectorAll(".reveal");
+
+  const io = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((e) => {
+        if (e.isIntersecting) e.target.classList.add("is-in");
+      });
+    },
+    { threshold: 0.12, rootMargin: "0px 0px -10% 0px" }
+  );
+
+  els.forEach((el) => io.observe(el));
 })();
+
 
